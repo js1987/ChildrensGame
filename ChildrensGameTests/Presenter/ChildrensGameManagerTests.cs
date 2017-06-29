1044831c4a8671bbd4666f4be8f335917db5c437
John Suen <Jsuen@danielshealth.com>
2017-06-29 17:34:23 +1000

    Cleaned up.

--- ChildrensGameTests/Presenter/ChildrensGameManagerTests.cs
+++ ChildrensGameTests/Presenter/ChildrensGameManagerTests.cs
@@ -11,15 +11,15 @@ namespace ChildrensGameTests.Presenter
     public class ChildrensGameManagerTests
     {
         [TestMethod]
-        [DataRow(3, 1, 3, new int[2] { 1, 2 })]
-        [DataRow(3, 2, 3, new int[2] { 2,1 })]
-        [DataRow(3, 3, 2, new int[2] { 3, 1 })]
-        [DataRow(3, 4, 2, new int[2] { 1, 3 })]
-        [DataRow(3, 5, 1, new int[2] { 2, 3 })]
-        [DataRow(3, 6, 1, new int[2] { 3, 2 })]
-        [DataRow(10, 3, 4, new int[9] { 3, 6, 9, 2, 7, 1, 8, 5, 10 })]        
-        [DataRow(10, 6, 3, new int[9] { 6, 2, 9, 7, 5, 8, 1, 10, 4 })]
-        [DataRow(10, 12, 10, new int[9] { 2, 5, 9, 6, 4, 8, 7, 3, 1 })]
+        [DataRow(3, 1, 3, new [] { 1, 2 })]
+        [DataRow(3, 2, 3, new [] { 2,1 })]
+        [DataRow(3, 3, 2, new [] { 3, 1 })]
+        [DataRow(3, 4, 2, new [] { 1, 3 })]
+        [DataRow(3, 5, 1, new [] { 2, 3 })]
+        [DataRow(3, 6, 1, new [] { 3, 2 })]
+        [DataRow(10, 3, 4, new [] { 3, 6, 9, 2, 7, 1, 8, 5, 10 })]        
+        [DataRow(10, 6, 3, new [] { 6, 2, 9, 7, 5, 8, 1, 10, 4 })]
+        [DataRow(10, 12, 10, new [] { 2, 5, 9, 6, 4, 8, 7, 3, 1 })]
         [DataRow(1, 1, 1, new int[0])]
         public void CalculateGameResult_PassInChildrenCountAndEliminateEach_ShouldReturnEexpectedResult(int childrenCount, int eliminateEach, int lastChild, int[] orderOfElimination)
         {
